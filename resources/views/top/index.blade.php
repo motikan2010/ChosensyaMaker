6db24085ebe1cc36b23ff41c47be1d0d98840958
KouheiAgena <motikan2010@yahoo.co.jp>
2019-01-03 06:31:46 +0900

    add 説明

--- resources/views/top/index.blade.php
+++ resources/views/top/index.blade.php
@@ -3,24 +3,41 @@
 @section('content')
   <div class="row">
     <div class="col-md-6 offset-md-3">
-      {!! Form::open(['url' => Helper::formAction('/create'), 'method' => 'post', 'files' => true]) !!}
-      @if ($errors->any())
-        <div class="alert alert-danger">
-          <ul>
-            @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-            @endforeach
-          </ul>
+      <div class="card">
+        <h5 class="card-header"></h5>
+        <div class="card-body">
+          {!! Form::open(['url' => Helper::formAction('/create'), 'method' => 'post', 'files' => true]) !!}
+          @if ($errors->any())
+            <div class="alert alert-danger">
+              <ul>
+                @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+                @endforeach
+              </ul>
+            </div>
+          @endif
+          <div class="form-group row">
+            {!! Form::label('file', '画像アップロード', ['id' => 'image-form', 'class' => 'control-label']) !!}
+            {!! Form::file('image', ['id' => 'image-form', 'class' => 'form-control-file']) !!}
+          </div>
+          <div class="form-group row">
+            {!! Form::submit('アップロード', ['class' => 'btn btn-primary']) !!}
+          </div>
+          {!! Form::close() !!}
         </div>
-      @endif
-      <div class="form-group row">
-        {!! Form::label('file', '画像アップロード', ['id' => 'image-form', 'class' => 'control-label']) !!}
-        {!! Form::file('image', ['id' => 'image-form', 'class' => 'form-control-file']) !!}
       </div>
-      <div class="form-group row">
-        {!! Form::submit('アップロード', ['class' => 'btn btn-primary']) !!}
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-6 offset-md-3">
+      <div class="card">
+        <h5 class="card-header">概要</h5>
+        <div class="card-body">
+          ※人物画像の背景が自動的にトリミングされます。
+          <img class="card-img-top img-thumbnail" src="/images/before.jpg" alt="Before image">
+          <img class="card-img-top img-thumbnail" src="/images/after.png" alt="After image">
+        </div>
       </div>
-      {!! Form::close() !!}
     </div>
   </div>
 @endsection
