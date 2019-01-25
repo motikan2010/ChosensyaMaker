bb29df4167707d559770eed012e3cc5dd49891c1
motikan2010 <motikan2010@yahoo.co.jp>
2019-01-26 01:36:08 +0900

    konvaを追加 #1

--- resources/views/top/create.blade.php
+++ resources/views/top/create.blade.php
@@ -2,25 +2,18 @@
 
 @section('content')
   <div class="row">
-    <div class="col-md-6 offset-md-3">
-      <div class="slider">
-        <span>縮小</span>
-        <input id="zoom-slider" type="range"/>
-        <span>拡大</span>
-      </div>
+    <div class="col-lg-6 offset-lg-3">
       <div class="monochrome">
         シルエット（β版）
         <input type="radio" name="monochrome-radio" value="1"/>ON
         <input type="radio" name="monochrome-radio" value="0" checked="checked"/>OFF
       </div>
       <div id="image-info" url="{{$imageUrl}}"></div>
-      <div id="input">
-        <canvas id="canvas" width="500" height="280"></canvas>
-      </div>
+      <div id="input"></div>
     </div>
   </div>
   <div class="row">
-    <div class="col-md-6 offset-md-3">
+    <div class="col-lg-6 offset-lg-3">
       <div class="form-group">
         <button class="btn btn-success" onclick="take()">画像を生成する</button>
         <a id="download">
@@ -37,6 +30,7 @@
 @endsection
 
 @section('javascript')
+  <script type="text/javascript" src="/js/konva.min.js"></script>
   <script type="text/javascript" src="/js/html2canvas.min.js"></script>
   <script type="text/javascript" src="/js/app.js"></script>
 @endsection
